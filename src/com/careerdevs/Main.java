2cfb52fdab1db0c27f03df103cb41f94ffee0380
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-07 22:35:24 -0500

    A-02-03 done

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -10,5 +10,24 @@ public class Main {
         int myAge= 30;
         int herAge = myAge;
         System.out.println(herAge);
+
+        /* Primitive types are used for storing simple values
+        Reference is used for storing complex objects
+         under primitive we have
+         byte  1
+         short  2
+         int  4
+         long  8
+         float  4
+         double 8
+         char  2
+           all words in orange are reserved key word in java
+        * */
+        byte age = 30;
+        long viewCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false
+
     }
 }
