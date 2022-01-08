8056af936aff5c0b824b5406cc8e03dae65f224c
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-07 22:16:03 -0500

    Initial commit

---
+++ src/com/careerdevs/Main.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int age = 30;
+//           age = 35;
+        int myAge= 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
